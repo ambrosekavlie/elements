e1e41f841936ef5463f32fb1f4f64d1196739497
Ambrose Kavlie <ambrosekavlie@gmail.com>
2025-03-10 09:39:54 -0700

    update test to import from the program

--- test/test_json_data.py
+++ test/test_json_data.py
@@ -1,5 +1,10 @@
 import unittest
 
+import sys
+sys.path.append('src')
+
+from elements import update_json_data
+
 import json
 import os
 import random
@@ -10,42 +15,20 @@ class TestJsonData(unittest.TestCase):
             os.remove("data.json")
         except FileNotFoundError:
             print("data.json file not found")
+        
+        highscore = 101
         for i in range(1000):
             # random number as time taken
             time_taken = random.randint(0, 100)
-            # try to open highscore.json file. If it doesn't exist, make it
-            try:
-                with open("data.json", 'r') as highscore_file:
-                    data = json.load(highscore_file)
-            except FileNotFoundError:
-                with open("data.json", 'w') as highscore_file:
-                    data = {
-                        "highscore" : time_taken+1
-                    }
-                    json.dump(data, highscore_file)
+            if time_taken < highscore:
+                highscore = time_taken
             
-            # time and highscore logic below:
-            
-            highscore = data["highscore"]
-            if time_taken < data["highscore"]:
-                # if time taken is less than highscore, set new highscore and print highscore message
-                data["highscore"] = time_taken
-                with open("data.json", 'w') as highscore_file:
-                    json.dump(data, highscore_file)
-                highscore = data["highscore"]
-                print(f"\nHIGHSCORE! You took {time_taken} seconds to complete the test.")
-            else:
-                # else, print normal message that is not highscore
-                # also print your previous highscore
-                print(f"\nYou took {time_taken} seconds to complete the test. Good job!")
-                highscore = data["highscore"]
-                print(f"Previous highscore: {highscore}")
+            update_json_data(time_taken)
                 
             # TEST
             with open("data.json", 'r') as highscore_file:
                 data = json.load(highscore_file)
                 self.assertEqual(data["highscore"], highscore)
-
         
 if __name__ == "__main__":
     unittest.main()
